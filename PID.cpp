d1736886015995a4ce1d53bf651a95e4a6bb35a0
p4pradeep <pradeep6002@gmail.com>
2017-09-17 18:36:23 +0000

    Add files via upload

--- PID.cpp
+++ PID.cpp
@@ -10,10 +10,10 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
-	this->Kp = Kp;
-	this->Kd = Kd;
-	this->Ki = Ki;
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+	Kp = Kp_;
+	Kd = Kd_;
+	Ki = Ki_;
 
 	p_error = 0;
 	i_error = 0;
--- main.cpp
+++ main.cpp
@@ -34,9 +34,9 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  double init_Kp = -1;
-  double init_Ki = 0;
-  double init_Kd = 0;
+  double init_Kp = -0.05;
+  double init_Ki = -0.5;
+  double init_Kd = -0.001;
   pid.Init(init_Kp, init_Ki, init_Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
